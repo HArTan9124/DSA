a406f1e3531e40f8423aba8d9289c8b249faeebb
Harshit Tandon <tandonharshit757@gmail.com>
2025-07-04 22:47:58 +0530

    commit1.2

---
+++ basics.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class basics {
+    public static void main(String args[]){
+
+        //Print 
+        System.out.println("Hello Word");
+        int a = 10;
+        int b = 25;
+        int c = 2*(a+b); 
+
+        System.out.println(c);
+
+        //Scanner
+        System.out.print("Enter Your Name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Your name is : "+ name);
+
+        System.out.print("Enter value of X: ");
+        int X = sc.nextInt();
+        System.out.print("Enter value of Y: ");
+        int Y = sc.nextInt();
+
+        int Z = X+Y;
+        System.out.println("X + Y =  " + Z);
+
+        //Conditional 
+        
+
+
+
+    }
+
+    
+}
