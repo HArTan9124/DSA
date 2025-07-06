b45958fb885a9b625471a5c796dbeb843f0af062
Harshit Tandon <tandonharshit757@gmail.com>
2025-07-06 09:40:12 +0530

    commit1.2

--- basics.java
+++ basics.java
@@ -26,11 +26,37 @@ public class basics {
         int Z = X+Y;
         System.out.println("X + Y =  " + Z);
 
-        //Conditional 
+        //Conditional  (if-else)
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age>=18) {
+            System.out.println("You Can Vote");
+        }else{
+            System.out.println("Sorry Better Luck Next time");
+        }
+
+        //Switch
+
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:System.out.println("Hello");
+            break;
+
+            case 2:System.out.println("Namaste");
+            break;
+
+            case 3:System.out.println("Radhe Radhe");
+            break;
+            
+            default: System.out.println("Invalid Button");
+                break;
+
+
+        }
+
+        //Loops
+
         
-
-
-
     }
 
     
