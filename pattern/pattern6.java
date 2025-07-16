54db1bcb4961dea9db934b983839f43510da02a9
Harshit Tandon <tandonharshit757@gmail.com>
2025-07-17 00:06:51 +0530

    17-07-25

---
+++ pattern/pattern6.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        for(int i=0 ;i<9;i++){
+            for(int j=0;j<9;j++){
+                if (j==i || j== 9 -i-1) {
+                System.out.print("#");
+                    
+                }else{
+                    System.out.print(" ");
+                } 
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
