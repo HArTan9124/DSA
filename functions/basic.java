9fdb7d17149104ebe1a17f81dbb188543670013b
Harshit Tandon <tandonharshit757@gmail.com>
2025-07-17 22:53:35 +0530

    17-07-2025

---
+++ functions/basic.java
@@ -0,0 +1,59 @@
+package functions;
+
+import java.util.*;
+
+public class basic {
+    public static void add(Integer a , Integer b){
+        Scanner sc = new Scanner(System.in);
+        int c = a + b;
+        System.out.println("Your Sum is " + c);
+        return;
+    }
+
+    public static void subtract(Integer a , Integer b){
+        Scanner sc = new Scanner(System.in);
+        int c = a - b;
+        System.out.println("Your Sum is " + c);
+        return;
+    }
+
+    public static void multiply(Integer a , Integer b){
+        Scanner sc = new Scanner(System.in);
+        int c = a * b;
+        System.out.println("Your Sum is " + c);
+        return;
+    }
+
+    public static void devide(Integer a , Integer b){
+        Scanner sc = new Scanner(System.in);
+        int c = a % b;
+        System.out.println("Your Sum is " + c);
+        return;
+    }
+
+    public static void choice(String[] args) {
+        
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter Your numbers");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        System.out.println("Enter your choice \n1)  add \n2) subtract \n3) multiply \n4) devide");
+        int i = sc.nextInt();
+        if (i==1) {
+                add(x,y);
+            }else if (i==2) {
+                subtract(x,y);
+            }else if (i==3) {
+                multiply(x,y);
+            }else if (i==4) {
+                devide(x,y);
+            }else{
+                System.out.println("Wrong choice");
+            }
+        sc.close();
+    }
+}
